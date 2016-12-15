d0a8e4ac739501fae782c64169f405fe84cbfbc0
Kelley Parker <kelley.parker07@gmail.com>
2016-12-15 08:38:09 -0600

    update

--- whats_your_name.py
+++ whats_your_name.py
@@ -1,3 +1,5 @@
+# In this python file, you type in your name and it writes it to a text file within the directory.  Raw_input accomplishes this where input does not
+
 name = raw_input("What is your name?")
 print("Your name is " + name + ".")
 print("This name has been written to a text file.")
