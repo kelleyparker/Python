5921c3745d4af3e0a2ad1c4a0c811f166185c2c0
Kelley Parker <kelley.parker07@gmail.com>
2022-03-29 20:12:14 -0500

    commit

--- windows_tests.py
+++ windows_tests.py
@@ -2,19 +2,33 @@
 
 import os
 import platform
+import shutil
 
 ### This line of code prints "howdy!" ###
-print("howdy")
+#print("howdy")
 
 ### Get current directory on Windows
-dir = os.getcwd()
-print(dir)
+#dir = os.getcwd()
+#print(dir)
 
 ### Use powershell to echo hello as test
-os.system("powershell.exe echo hello!")
+#os.system("powershell.exe echo hello!")
 
 ### Use powershell to get registered user on computer
-os.system("powershell.exe Get-ComputerInfo -Property OSRegisteredUser")
+#os.system("powershell.exe Get-ComputerInfo -Property OSRegisteredUser")
 
 ### List disks on computer on powershell
-os.system("powershell.exe get-disk")
+#os.system("powershell.exe get-disk")
+
+### Create subfolders of NFL Teams on C:\
+os.makedirs('C:/NFL Teams/NFC/NFC East', exist_ok=True)
+os.makedirs('C:/NFL Teams/NFC/NFC West', exist_ok=True)
+os.makedirs('C:/NFL Teams/NFC/NFC North', exist_ok=True)
+os.makedirs('C:/NFL Teams/NFC/NFC South', exist_ok=True)
+os.makedirs('C:/NFL Teams/AFC/AFC East', exist_ok=True)
+os.makedirs('C:/NFL Teams/AFC/AFC West', exist_ok=True)
+os.makedirs('C:/NFL Teams/AFC/AFC North', exist_ok=True)
+os.makedirs('C:/NFL Teams/AFC/AFC South', exist_ok=True)
+
+# Remove these folders I created on C:\
+#shutil.rmtree('C:/NFL Teams/', ignore_errors=True)
