acbce98dbc9c527093557339b4391e8e6614c283
Kelley Parker <kelley.parker07@gmail.com>
2025-07-16 13:16:38 -0500

    Update dice.py
    corrected print commands on lines 8 and 9

--- dice.py
+++ dice.py
@@ -3,10 +3,10 @@ import random
 def roll():
     decision = input("Do you want to roll 1 die, or 2 dice?")
     if decision == 1:
-        print random.randint(1,6)
+        print(random.randint(1,6))
     elif decision == 2:
-        print random.randint(1,6)
-        print random.randint(1,6)
+        print(random.randint(1,6))
+        print(random.randint(1,6))
     else:
         print("Wrong answer! Program ends now!!")
 
